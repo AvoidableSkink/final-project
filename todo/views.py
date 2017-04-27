163935f051e5be6a5d35068d0336c84fda152632
Kimberly Lindholm <klindholm7@gmail.com>
2017-04-27 21:59:30 +0000

    add basic template

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
+
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    todos = Todo.objects.all()
+    context = {'todos':todos}
+    return render(request, 'index.html', context)
