4300c84d5ea8e5f0f4e853bb3cf728a62d1b2a77
Kimberly Lindholm <klindholm7@gmail.com>
2017-04-28 05:01:32 +0000

    change id to bug

--- todo/static/todo/javascript/bugs.js
+++ todo/static/todo/javascript/bugs.js
@@ -4,7 +4,7 @@ var x = Math.floor(Math.random()*300);
 var y = Math.floor(Math.random()*300);
 
 
-var obj = document.getElementById("emotion");
+var obj = document.getElementById("bug");
 
 obj.style.top = x + "px";
 obj.style.left = y + "px";
