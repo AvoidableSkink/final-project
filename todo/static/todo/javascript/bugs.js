066604bf378176846bf51a5d3b9b70d7e59a4434
Kimberly Lindholm <klindholm7@gmail.com>
2017-04-28 23:01:29 +0000

    images are placed in random place upon loading

--- todo/static/todo/javascript/bugs.js
+++ todo/static/todo/javascript/bugs.js
@@ -1,7 +1,7 @@
 function changeImg()
 {
-var x = Math.floor(Math.random()*300);
-var y = Math.floor(Math.random()*300);
+var x = Math.floor(Math.random()*500);
+var y = Math.floor(Math.random()*500);
 
 
 var obj = document.getElementById("bug");
@@ -11,3 +11,38 @@ obj.style.left = y + "px";
 
 
  }
+ 
+function randomlyPlace(){
+  var w=document.body.offsetWidth;
+  var h=document.body.offsetHeight;
+  var rd=document.getElementsByTagName('img');
+
+for(var c=0;c<rd.length;c++) {
+if(rd[c].className=='random') {
+   var xCoord=Math.floor(Math.random()*w);
+   var yCoord=Math.floor(Math.random()*h);
+
+if(xCoord>=w-rd[c].offsetWidth-10){
+   xCoord=w-rd[c].offsetWidth-10;
+ }
+if(xCoord<=10){
+   xCoord=10;
+ }
+
+if(yCoord>=h-rd[c].offsetHeight-10){
+   yCoord=h-rd[c].offsetHeight-10;
+ }
+if(yCoord<=10){
+   yCoord=250;
+ }
+
+   rd[c].style.left=xCoord+'px';
+   rd[c].style.top=yCoord+'px';
+   }
+  }
+ }
+window.addEventListener?
+window.addEventListener('load',randomlyPlace,false):
+window.attachEvent('onload',randomlyPlace);
+
+randomlyPlace();
